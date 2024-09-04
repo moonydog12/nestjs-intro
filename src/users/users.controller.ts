d2a398e852e24d0a5e1e6b125baefcfb8fb48e66
moonydog12 <luo0112r@gmail.com>
2024-09-04 12:50:58 +0800

    feat-Convert object to an instance of DTO & using it with params

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
   DefaultValuePipe,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { GetUsersParamDto } from './dtos/get-users-param.dto';
 
 @Controller('users')
 export class UsersController {
@@ -20,18 +21,17 @@ export class UsersController {
    */
   @Get('/:id?')
   public getUsers(
-    @Param('id', ParseIntPipe) id: number | undefined,
+    @Param() getUsersParamDto: GetUsersParamDto,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
-    console.log(limit);
-    console.log(page);
+    console.log(getUsersParamDto);
     return 'You sent a get request to users endpoint';
   }
 
   @Post()
   public createUsers(@Body() createUserDto: CreateUserDto) {
-    console.log(createUserDto);
+    console.log(createUserDto instanceof CreateUserDto);
     return 'You sent a post request';
   }
 }
