0171f57b0a5d815ae613841348e1fa7b6128e0dd
moonydog12 <luo0112r@gmail.com>
2024-09-09 14:12:41 +0800

    feat:Enable Swagger & add configuration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // use validation pipes globally
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -11,6 +14,26 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  /**
+   * Swagger configuration
+   */
+  const config = new DocumentBuilder()
+    .setTitle('NestJs Masterclass - Blog app API')
+    .setDescription('Use the base API as http://localhost:3000')
+    .setTermsOfService('http://localhost:3000/terms-of-service')
+    .setLicense(
+      'MIT License',
+      'https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt',
+    )
+    .addServer('http://localhost:3000')
+    .setVersion('1.0')
+    .build();
+
+  // Instantiate document
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
